d53407863cb621255674fe14ae30e868cd27fb87
Saad Ali Khan <saadalikhan042@gmail.com>
2024-04-28 20:24:30 +0500

    categories complete(static)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+import Categories from "./components/Categories";
 export default function Home() {
-  return <main className="text-airbnb">Airbnb</main>;
+  return (
+    <main className="max-w-[1500px] mx-auto px-6">
+      <Categories />
+    </main>
+  );
 }

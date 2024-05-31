9dc90649e196c4b446d5a6f114bb6fe5dcc9a4c0
Saad Ali Khan <saadalikhan042@gmail.com>
2024-05-31 18:56:01 +0500

    backend and frontend connected

--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -1,10 +1,37 @@
+"use client";
 import PropertyListItem from "./PropertyListItem";
+import { useState, useEffect } from "react";
+
+export type PropertyType = {
+  id: string;
+  title: string;
+  image_url: string;
+  price_per_night: number;
+};
+
 const PropertyList = () => {
+  const [properties, setProperties] = useState<PropertyType[]>([]);
+  const getProperties = async () => {
+    const url = "http://localhost:8000/api/properties/";
+    await fetch(url, {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        setProperties(json.data);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
+  useEffect(() => {
+    getProperties();
+  }, []);
   return (
     <>
-      <PropertyListItem />
-      <PropertyListItem />
-      <PropertyListItem />
+      {properties.map((property) => (
+        <PropertyListItem key={property.id} property={property} />
+      ))}
     </>
   );
 };
